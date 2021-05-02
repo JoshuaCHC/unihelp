d0da2ab4b13524fbcb0b36c0210f5c0c7b97fd4b
Tatum Botha <tatum.botha7@gmail.com>
2021-05-02 15:30:05 +0800

    adding new routes for modules

--- app/routes.py
+++ app/routes.py
@@ -13,21 +13,34 @@ def about():
 @app.route('/rankings')
 def rankings():
     return render_template('Rankings.html')	
-@app.route('/login/module1')
+
+@app.route('/login/module1/content')
 def mod1():
     return render_template('Module1.html')
 
-@app.route('/login/module2')
+@app.route('/login/module2/content')
 def mod2():
     return render_template('Module2.html')
 
-@app.route('/login/module3')
+@app.route('/login/module3/content')
 def mod3():
     return render_template('Module3.html')
 
+@app.route('/login/module1/quiz')
+def quiz1():
+    return render_template('Module1_quiz.html')
 
+@app.route('/login/module2/quiz')
+def quiz2():
+    return render_template('Module2_quiz.html')
 
+@app.route('/login/module3/quiz')
+def quiz3():
+    return render_template('Module3_quiz.html')
 
+@app.route('/login/modules')
+def home_mod():
+    return render_template('home_mod.html')
 
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -37,7 +50,7 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid user'
         else:
-            return redirect(url_for('mod1'))
+            return redirect(url_for('home_mod'))
     return render_template('Login.html', error=error)
 
 @app.route('/modules')
