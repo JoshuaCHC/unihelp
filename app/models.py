7fa30148bc52fb832ce9977703bc9e2a49e67020
joshc2020 <74691506+joshc2020@users.noreply.github.com>
2021-05-02 17:58:25 +0800

    Creating login and adding db functionality

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -14,6 +16,7 @@ class User(db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        print(password, check_password_hash(self.password_hash,password))
         return check_password_hash(self.password_hash, password)
 
 
@@ -26,3 +29,7 @@ class Marks(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     def __repr__(self):
         return '<User {} got marks: 1: {} 2: {} 3: {}>'.format(self.user_id, mod1,mod2,mod3)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from app import app
-from app.forms import LoginForm
-
+from app import app,db
+from app.forms import LoginForm, RegistrationForm
+from werkzeug.urls import url_parse
+from flask_login import login_required, logout_user, current_user, login_user
+from app.models import User
 @app.route("/")
 def homePage():
     return render_template('HomePage.html')
@@ -14,14 +16,15 @@ def about():
 @app.route('/rankings')
 def rankings():
     return render_template('Rankings.html')	
+@login_required
 @app.route('/login/module1')
 def mod1():
     return render_template('Module1.html')
-
+@login_required
 @app.route('/login/module2')
 def mod2():
     return render_template('Module2.html')
-
+@login_required
 @app.route('/login/module3')
 def mod3():
     return render_template('Module3.html')
@@ -29,22 +32,66 @@ def mod3():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = []
+    if current_user.is_authenticated:
+        return redirect(url_for('mod1'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('homePage'))
-    return render_template('login.html', title='Sign In', form=form)
-# @app.route('/login', methods = ['GET', 'POST'])
-# def login():
-#     error = ''
-#     if request.method == 'POST':
-#         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-#             error = 'Invalid user'
-#         else:
-#             return redirect(url_for('mod1'))
-#     return render_template('Login.html', error=error)
+        user = User.query.filter_by(username=form.username.data).first()
+        bool = True
+        if user is None: 
+            print('No user')
+            bool = False
+            error.append('Invalid username')
+            return render_template('login.html', form=form, errors= error)
 
+        if not user.check_password(form.password.data):
+            print(form.password.data)
+            error.append('Invalid password')
+            return render_template('login.html', form=form, errors= error)
+
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '' or next_page == '/':
+            next_page = url_for('homePage')
+        return redirect(url_for('mod1'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('homePage'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = []
+    err = False
+    print('trying reg')
+    if current_user.is_authenticated:
+        return redirect(url_for('homePage'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print('Form is good?')
+        if(User.query.filter_by(username=form.username.data).first() != None):
+            #Could try use regex to suggest new ones.
+            error.append('Username is taken, please try another.')
+            err = True
+        if(User.query.filter_by(email=form.email.data).first() != None):
+            #Suggest forgot password?
+            error.append('There is already an account with that email address.')
+            err = True
+        if(err):
+            return render_template('registerform.html', title='Register', form=form, errors = error)
+
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('homePage'))
+    return render_template('registerform.html', title='Register', form=form)
+
+@login_required
 @app.route('/modules')
 def modules():
     return render_template('Modules.html')
